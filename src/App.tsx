50d3aeeaa7d257b97ac159f60e8aa6e1024a2d0a
Juan Carlos Rodríguez <jcharlyrodriguez1992@gmail.com>
2025-10-12 20:28:24 +0200

    Add fadeIn animation and update Onboarding component styling
    - Introduced a fadeIn animation in animations.css for smoother transitions.
    - Updated Onboarding component to utilize fadeIn effect for elements.
    - Enhanced layout and styling of Onboarding steps, including updated titles and paragraphs in localization.
    - Modified App component to conditionally render Onboarding based on onboarding completion status.

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { useThemeStore } from './stores/themeStore'
 import ThemeToggle from './components/ThemeToggle/ThemeToggle'
 import LanguageToggle from './components/LanguageToggle/LanguageToggle'
 import Onboarding from './components/Onboarding/Onboarding'
+import { useOnboardingStore } from './stores/onboardingStore'
 
 const App = () => {
   const { theme } = useThemeStore()
+  const {isOnboardingComplete} = useOnboardingStore()
 //  const { t } = useTranslation()
 
   return (
-    <div className={`${theme} h-dvh overflow-hidden bg- d-200 dark:bg-neutral-900 transition-all duration-1000 flex flex-col dark:text-neutral-200 text-neutral-700`}>
+    <div className={`${theme} h-dvh overflow-hidden bg-gradient-to-b from-orange-400/60 to-orange-600/60 d-200 dark:bg-neutral-900 transition-all duration-1000 flex flex-col dark:text-neutral-200 text-neutral-700`}>
 
-      <nav id='navbar' className='flex gap-4 pe-3 items-center shadow-lg'>
+      <nav id='navbar' className='flex gap-4 pe-3 items-center shadow-lg bg-neutral-200 dark:bg-neutral-900'>
         <div className='flex-1'>
           <img src={theme === "dark" ? "https://file.notion.so/f/f/9e9c490f-7f6e-4f2e-9796-027b83925332/39325554-76df-4d6d-b357-38d5dbf7f592/RingTwice-logo-negative-rgb.svg?table=block&id=4899f51c-7cc1-4479-9f69-1c48322410af&spaceId=9e9c490f-7f6e-4f2e-9796-027b83925332&expirationTimestamp=1760306400000&signature=xmo5H4hsDNkQBTevZp2cb4PtmvduS4X5w-SdJj92Z1I&downloadName=RingTwice-logo-negative-rgb.svg" : "https://file.notion.so/f/f/9e9c490f-7f6e-4f2e-9796-027b83925332/2e35dd39-4eaa-4629-9a41-fc3f63800eef/RingTwice-logo-main-rgb.svg?table=block&id=f556463a-61e2-4120-9615-12f238c6b6fe&spaceId=9e9c490f-7f6e-4f2e-9796-027b83925332&expirationTimestamp=1760306400000&signature=a85hilKeXEk1R16dXLhSn48bw868wordu62VnuQPOgY&downloadName=RingTwice-logo-main-rgb.svg"} alt="" className='w-40 starting:opacity-0 transition-all duration-1000'/>
         </div>
@@ -20,11 +22,11 @@ const App = () => {
         <ThemeToggle />
 
       </nav>
-      <main id='content' className='flex-1 flex flex-col px-3 py-4'>
+      <main id='content' className='flex-1 flex flex-col  overflow-hidden'>
         
 
-       
-          <Onboarding/>
+       {!isOnboardingComplete && <Onboarding/>}
+          
         
       </main>
 
--- src/components/Onboarding/Onboarding.tsx
+++ src/components/Onboarding/Onboarding.tsx
@@ -3,27 +3,38 @@ import { useTranslation } from 'react-i18next'
 
 const Onboarding = () => {
 
-    const { currentStep, prevStep, nextStep } = useOnboardingStore()
+    const { currentStep, prevStep, nextStep, completeOnboarding } = useOnboardingStore()
     const { t } = useTranslation()
+    
 
 
     return (
-        <div className='border flex-1 flex flex-col'>
+        <div className=' relative flex-1 flex flex-col gap-6 px-3 py-4 overflow-hidden max-w-xl mx-auto'>
 
-            <div className='flex gap-2'>
-                <div className='flex-1'>
-                    <p className='text-lg font-bold'>{t(`onboarding.title_step${currentStep}`)}</p>
-                </div>
-                <div>SKIP</div>
+            <div className='flex gap-2 justify-end z-1 fadeIn' style={{animationDelay: "0.6s"}}>
+               
+                <button className='border px-2' onClick={completeOnboarding}>SKIP</button>
             </div>
-            <div className='flex-3'>Graphic</div>
-            <div className='flex-1'>Paragraphs</div>
-            <div className='flex items-center justify-between'>
-            <button onClick={prevStep}>prev step</button>
-            <button onClick={nextStep} disabled={currentStep >= 3}>next-step</button>
+            <div className='flex absolute inset-0 z-0'>
+
+                {currentStep === 1 && <img src="https://images.unsplash.com/photo-1611095785020-1ba3dd228ea7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1287" alt="" className='aspect-[16/9] mask-b-from-0% mask-b-to-80% object-cover object-top fadeIn' />}
+                {currentStep === 2 && <img src="https://images.unsplash.com/photo-1747930838628-75fa744f6b15?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1287" alt="" className='aspect-[16/9] mask-b-from-0% mask-b-to-80% object-cover object-top fadeIn' />}
+                {currentStep === 3 && <img src="https://images.unsplash.com/photo-1714575600356-6635434699f8?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=3094" alt="" className='aspect-[16/9] mask-b-from-0% mask-b-to-80% object-cover object-top fadeIn' />}
+                
             </div>
-            
-            
+            <div className='flex-col flex gap-6 z-1 flex-1 justify-end fadeIn' style={{animationDelay: "0.2s"}}>
+                <p className='text-4xl font-bold'>{t(`onboarding.title_step${currentStep}`)}</p>
+                <p className='text-xl'>{t(`onboarding.paragraph_step${currentStep}`)}</p>
+            </div>
+            <div className='flex  items-center justify-between z-1 gap-4 fadeIn' style={{animationDelay: "0.6s"}}>
+                { currentStep > 1 &&
+
+                <button className='border rounded-full px-4 py-2 w-full' onClick={prevStep}>prev step</button>
+                }
+                <button className='border rounded-full px-4 py-2 w-full' onClick={currentStep === 3 ? completeOnboarding : nextStep}>next-step</button>
+            </div>
+
+
         </div>
     )
 }
