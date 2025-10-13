08c4510af3359425c015dcb3299911a7ab48a000
Juan Carlos Rodríguez <jcharlyrodriguez1992@gmail.com>
2025-10-13 10:16:05 +0200

    Add form fields and layout to Form component for enhanced user input
    - Introduced multiple select dropdowns for categories, sub-categories, and sub-activities.
    - Added input fields for file uploads, address, contact information, and additional details.
    - Implemented checkboxes for materials and tools provision.
    - Enhanced layout with a summary section for better organization of user inputs.

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,13 +2,60 @@ import { useOnboardingStore } from '../../stores/onboardingStore'
 
 const Form = () => {
 
+const categories = [];
+const subCategories=[];
+const subActivities=[]
+
     const {resetOnboarding} = useOnboardingStore()
 
 
   return (
-    <div className='felx flex-col gap-4 fadeIn'>
-        <p>THE FORM</p>
+    <div className='flex flex-col gap-4 fadeIn border flex-1 overflow-hidden'>
         <button onClick={resetOnboarding}>RESET ONBOARDING</button>
+
+        <div className='flex items-center gap-4'>
+            <div className='flex flex-col gap-8 overflow-auto flex-1'>
+                <select name="" id="">
+                    <option value="">Categories</option>
+                </select>
+                <select name="" id="">
+                    <option value="">Sub-category</option>
+                </select>
+                <select name="" id="">
+                    <option value="">Sub-Activity</option>
+                </select>
+
+                <input type="file" name="" id="" accept='img/jpg, img/jpeg, img/png, img/heic' />
+                <textarea name="" id="" placeholder='Extra details'></textarea>
+                <hr />
+                <input type="text" name="" id="" placeholder='Address' />
+                <select name="" id="">
+                    <option value="">Type of place</option>
+                </select>
+                <label htmlFor="">
+                    <input type="checkbox" name="" id="" />
+                    <p>Materials provided?</p>
+                </label>
+                <label htmlFor="">
+                    <input type="checkbox" name="" id="" />
+                    <p>Tools provided?</p>
+                </label>
+                <input type="number" name="" id="" placeholder='People required?' />
+                <textarea name="" id="" placeholder='Extra details (how to enter, parking, etc.)'></textarea>
+
+                <hr />
+
+                <input type="text" name="" id="" placeholder='Full name' />
+                <input type="tel" name="" id="" placeholder='Phone number' />
+                <input type="email" name="" id="" placeholder='Email' />
+
+                
+
+            </div>
+            <div>
+                SUMMARY
+            </div>
+        </div>
     </div>
   )
 }
