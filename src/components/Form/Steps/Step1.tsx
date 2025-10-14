c940ccdb270f0c12b8ded1e349ebb3ce71821668
Juan Carlos Rodríguez <jcharlyrodriguez1992@gmail.com>
2025-10-14 15:17:46 +0200

    fix unused stuff

---
+++ src/components/Form/Steps/Step1.tsx
@@ -0,0 +1,14 @@
+
+const Step1 = () => {
+  return (
+
+    //Category selection
+    //Subcategory selection
+    //Subactivity selection
+    // Description
+    //Photo picker / DropZone
+    <div>Step1</div>
+  )
+}
+
+export default Step1
